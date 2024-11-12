6199f7bcd72eebddf18dbbae894dbb16feb8d811
hudzg <hungbim004@gmail.com>
2024-11-12 22:59:25 +0700

    fix booking

--- Frontend/QAirline/src/Component/Booking/BookingCard.jsx
+++ Frontend/QAirline/src/Component/Booking/BookingCard.jsx
@@ -26,13 +26,25 @@ const BookingCard = () => {
       </div>
       <div className="w-[60%] flex">
         <div className="w-[100%]">
+          <Typography
+            sx={{
+              // background: "linear-gradient(to right, #B993D6, #8CA6DB)",
+              backgroundColor: "#B993D6",
+              height: "0.5rem",
+            }}
+          ></Typography>
           <Accordion className="w-[100%]">
             <AccordionSummary
               className="w-[100%]"
               expandIcon={<ArrowDropDownIcon />}
             >
               <div className="w-[100%]">
-                <Typography align="center" variant="h5" gutterBottom>
+                <Typography
+                  align="center"
+                  variant="h5"
+                  gutterBottom
+                  color={"#B993D6"}
+                >
                   Phổ thông
                 </Typography>
                 <Typography align="center" gutterBottom>
@@ -48,9 +60,45 @@ const BookingCard = () => {
               </div>
             </AccordionSummary>
             <AccordionDetails>
-              <div className="flex justify-around w-[100%]">
-                <Typography></Typography>
+              <div className="w-[100%] space-y-2">
+                <div className="flex space-x-2">
+                  <AirplaneTicketIcon />
+                  <div>
+                    <Typography>Hoàn vé</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      Không được phép
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <LuggageIcon />
+                  <div>
+                    <Typography>Hành lý ký gửi</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      23 kg
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <WorkIcon />
+                  <div>
+                    <Typography>Hành lý xách tay</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      12 kg
+                    </Typography>
+                  </div>
+                </div>
                 <Button
+                  fullWidth
                   variant="outlined"
                   // sx={{
                   //   // p: "1rem",
@@ -64,14 +112,26 @@ const BookingCard = () => {
           </Accordion>
         </div>
         <div className="w-[100%]">
+          <Typography
+            sx={{
+              // background: "linear-gradient(to right, #B993D6, #8CA6DB)",
+              backgroundColor: "#A2A6DB",
+              height: "0.5rem",
+            }}
+          ></Typography>
           <Accordion className="w-[100%]">
             <AccordionSummary
               className="w-[100%]"
               expandIcon={<ArrowDropDownIcon />}
             >
               <div className="w-[100%]">
-                <Typography align="center" variant="h5" gutterBottom>
-                  Phổ thông
+                <Typography
+                  align="center"
+                  variant="h5"
+                  gutterBottom
+                  color={"#A2A6DB"}
+                >
+                  Thương gia
                 </Typography>
                 <Typography align="center" gutterBottom>
                   5.000.000 VND
@@ -86,8 +146,45 @@ const BookingCard = () => {
               </div>
             </AccordionSummary>
             <AccordionDetails>
-              <div className="flex justify-around w-[100%]">
+              <div className="w-[100%] space-y-2">
+                <div className="flex space-x-2">
+                  <AirplaneTicketIcon />
+                  <div>
+                    <Typography>Hoàn vé</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      Không được phép
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <LuggageIcon />
+                  <div>
+                    <Typography>Hành lý ký gửi</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      23 kg
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <WorkIcon />
+                  <div>
+                    <Typography>Hành lý xách tay</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      12 kg
+                    </Typography>
+                  </div>
+                </div>
                 <Button
+                  fullWidth
                   variant="outlined"
                   // sx={{
                   //   // p: "1rem",
@@ -101,14 +198,27 @@ const BookingCard = () => {
           </Accordion>
         </div>
         <div className="w-[100%]">
-          <Accordion className="w-[100%]">
+          <Typography
+            sx={{
+              // background: "linear-gradient(to right, #B993D6, #8CA6DB)",
+              backgroundColor: "#8CA6DB",
+              height: "0.5rem",
+            }}
+          ></Typography>
+          <Accordion className="w-[100%]" disabled={false}>
+            {/* <Accordion className="w-[100%]"> */}
             <AccordionSummary
               className="w-[100%]"
               expandIcon={<ArrowDropDownIcon />}
             >
               <div className="w-[100%]">
-                <Typography align="center" variant="h5" gutterBottom>
-                  Phổ thông
+                <Typography
+                  align="center"
+                  variant="h5"
+                  gutterBottom
+                  color={"#8CA6DB"}
+                >
+                  Hạng nhất
                 </Typography>
                 <Typography align="center" gutterBottom>
                   5.000.000 VND
@@ -123,8 +233,45 @@ const BookingCard = () => {
               </div>
             </AccordionSummary>
             <AccordionDetails>
-              <div className="flex justify-around w-[100%]">
+              <div className="w-[100%] space-y-2">
+                <div className="flex space-x-2">
+                  <AirplaneTicketIcon />
+                  <div>
+                    <Typography>Hoàn vé</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      Không được phép
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <LuggageIcon />
+                  <div>
+                    <Typography>Hành lý ký gửi</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      23 kg
+                    </Typography>
+                  </div>
+                </div>
+                <div className="flex space-x-2">
+                  <WorkIcon />
+                  <div>
+                    <Typography>Hành lý xách tay</Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      12 kg
+                    </Typography>
+                  </div>
+                </div>
                 <Button
+                  fullWidth
                   variant="outlined"
                   // sx={{
                   //   // p: "1rem",
