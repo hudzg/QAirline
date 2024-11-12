a38014b6ec149342e1ea13b243c1aaeef5c43f96
22021195BangVanChien <chienlgend24032004@gmail.com>
2024-11-12 21:52:04 +0700

    add feedback

--- Frontend/QAirline/src/Routers/CustomerRoute.jsx
+++ Frontend/QAirline/src/Routers/CustomerRoute.jsx
@@ -4,6 +4,7 @@ import Auth from "../Component/Auth/Auth";
 import Footer from "../Component/Footer/Footer";
 import Home from "../Component/Home/Home";
 import Navbar from "../Component/Navbar/Navbar";
+import Feedback from "../Component/Feedback/Feedback";
 
 const CustomerRoute = () => {
   return (
@@ -11,6 +12,7 @@ const CustomerRoute = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/feedback" element={<Feedback />}></Route>
       </Routes>
       <Auth />
       <Footer />
