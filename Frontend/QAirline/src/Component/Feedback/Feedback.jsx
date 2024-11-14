36683a4a2e2fafc6b7209b2cc70690b972a4fefc
22021195BangVanChien <chienlgend24032004@gmail.com>
2024-11-14 22:48:29 +0700

    feedback

--- Frontend/QAirline/src/Component/Feedback/Feedback.jsx
+++ Frontend/QAirline/src/Component/Feedback/Feedback.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
-
+import { Rating, Typography, Button } from "@mui/material";
 const Feedback = () => {
   return (
-    <div className="">Chien 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti omnis molestiae ratione sed voluptatem recusandae architecto eveniet tempora ad consequatur, numquam suscipit quisquam, quas laudantium pariatur animi autem quaerat rerum.
+    <div className="mx-auto items-center justify-center mt-24 mb-5 w-[50vw]">
+      <div className="mb-4">
+        <Typography variant="h6">Phản hồi của khách hàng về dịch vụ của chuyến bay</Typography>
+      </div>
+      <div className="mb-4">
+        <textarea className="w-[466px] h-[200px] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] "></textarea>
+      </div>
+      <div className="mb-4">
+        <Rating name="half-rating" defaultValue={5} precision={1} size="large">
+        </Rating>
+      </div>
+      <div>
+        <Button className=""
+          variant="contained"
+          sx={{
+            background: "linear-gradient(to right, #B993D6, #8CA6DB)",
+          }}>Submit</Button>
+      </div>
     </div>
   );
 };
