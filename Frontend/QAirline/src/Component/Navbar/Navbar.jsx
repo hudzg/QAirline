b1a205a6627562542aca1cc7b0956e2e536733af
hudzg <hungbim004@gmail.com>
2024-11-14 09:10:10 +0700

    update navbar

--- Frontend/QAirline/src/Component/Navbar/Navbar.jsx
+++ Frontend/QAirline/src/Component/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Divider } from "@mui/material";
+import { Avatar, Button, Divider } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,21 +7,49 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 bg-white z-50">
       <div className="py-5 px-5 lg:px-20 flex justify-between">
-        <div className="cursor-pointer font-semibold text-2xl">QAirline</div>
-        <div className="space-x-4">
-          <Button onClick={() => navigate("/account/login")}>Đăng nhập</Button>
-          <Button
-            // className="bg-gradient-to-r from-cyan-500 to-blue-500"
-            variant="contained"
-            onClick={() => navigate("/account/register")}
-            sx={{
-              // p: "1rem",
-              background: "linear-gradient(to right, #B993D6, #8CA6DB)",
-            }}
-          >
-            Đăng ký
-          </Button>
-        </div>
+        {false ? (
+          <>
+            <div className="cursor-pointer font-semibold text-2xl">
+              QAirline
+            </div>
+            <div className="space-x-4">
+              <Button onClick={() => navigate("/account/login")}>
+                Đăng nhập
+              </Button>
+              <Button
+                // className="bg-gradient-to-r from-cyan-500 to-blue-500"
+                variant="contained"
+                onClick={() => navigate("/account/register")}
+                sx={{
+                  // p: "1rem",
+                  background: "linear-gradient(to right, #B993D6, #8CA6DB)",
+                }}
+              >
+                Đăng ký
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="cursor-pointer font-semibold text-2xl">
+              QAirline
+            </div>
+            <div className="flex justify-between w-[40%] items-center">
+              <div className="cursor-pointer font-semibold text-xl">
+                Trang chủ
+              </div>
+              <div className="cursor-pointer font-semibold text-xl">
+                Chuyến bay
+              </div>
+              <div className="cursor-pointer font-semibold text-xl">
+                Phản hồi
+              </div>
+            </div>
+            <div>
+              <Avatar sx={{ bgcolor: "primary.main" }}>H</Avatar>
+            </div>
+          </>
+        )}
       </div>
       <Divider />
     </div>
